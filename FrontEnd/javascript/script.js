18d02efead91b85e3d8169b0fb12489271a5a6ba
Kévin Holleville <archercelest@gmail.com>
2025-01-28 15:24:34 +0100

    Add filter

--- FrontEnd/javascript/script.js
+++ FrontEnd/javascript/script.js
@@ -1,5 +1,31 @@
-async function getApi() {
+async function getWorks() {
   const url = "http://localhost:5678/api/works";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    for (let i = 0; i < json.length; i++) {
+      createFigure(json[i]);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+getWorks();
+
+function createFigure(data) {
+  const figure = document.createElement("figure");
+  figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+				<figcaption>${data.title}</figcaption>`;
+
+  document.querySelector(".gallery").append(figure);
+}
+
+async function getCategories() {
+  const url = "http://localhost:5678/api/categories";
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -9,18 +35,19 @@ async function getApi() {
     const json = await response.json();
     console.log(json);
     for (let i = 0; i < json.length; i++) {
-      createFigure(json[i]);
+      createFilter(json[i]);
     }
+
   } catch (error) {
     console.error(error.message);
   }
 }
-getApi();
 
-function createFigure(data) {
-  const figure = document.createElement("figure");
-  figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
-				<figcaption>${data.title}</figcaption>`;
+getCategories();
 
-  document.querySelector(".gallery").append(figure);
+function createFilter(data) {
+  const div = document.createElement("div");
+  div.innerHTML = data.name;
+
+  document.querySelector(".filter-container").append(div);
 }
