2e5c632330cdd7b6a4096fb7c2478ece53fc37aa
Kévin Holleville <archercelest@gmail.com>
2025-01-28 14:22:30 +0100

    create figure with js and loop for show all images

--- FrontEnd/javascript/script.js
+++ FrontEnd/javascript/script.js
@@ -8,9 +8,19 @@ async function getApi() {
 
     const json = await response.json();
     console.log(json);
+    for (let i = 0; i < json.length; i++) {
+      createFigure(json[i]);
+    }
   } catch (error) {
     console.error(error.message);
   }
 }
-
 getApi();
+
+function createFigure(data) {
+  const figure = document.createElement("figure");
+  figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+				<figcaption>${data.title}</figcaption>`;
+
+  document.body.append(figure);
+}
