86c15bce70fc74afa26e459ebe8521792438f050
Kévin Holleville <archercelest@gmail.com>
2025-02-04 15:02:50 +0100

    start modal

---
+++ FrontEnd/javascript/modal.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const editModeBtn = document.querySelector(".edit-mode");
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  const closeBtn = document.querySelector(".close-btn");
+
+  editModeBtn.addEventListener("click", function() {
+      modal.style.display = "block";
+      overlay.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", function() {
+      modal.style.display = "none";
+      overlay.style.display = "none";
+  });
+
+  overlay.addEventListener("click", function() {
+      modal.style.display = "none";
+      overlay.style.display = "none";
+  });
+});
+
+
+
+
+/* <i class="fa-solid fa-trash"></i> */
