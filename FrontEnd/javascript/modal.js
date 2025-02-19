113fd74cad532692316f3e978d53b3e8933902aa
Kévin Holleville <archercelest@gmail.com>
2025-02-19 16:51:56 +0100

    modal working but not the layout

--- FrontEnd/javascript/modal.js
+++ FrontEnd/javascript/modal.js
@@ -1,37 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const modifyMode = document.querySelector(".modify");
-  const modal = document.querySelector(".modal");
-  const overlay = document.querySelector(".overlay");
-  const closeButtons = document.querySelectorAll(".close-btn");
-  const addPicture = document.getElementById("addPicture");
+document.addEventListener("DOMContentLoaded", function () {
+    const modifyMode = document.querySelector(".modify");
+    const modal = document.querySelector(".modal");
+    const overlay = document.querySelector(".overlay");
+    const closeButtons = document.querySelectorAll(".close-btn");
+    const addPicture = document.getElementById("addPicture");
+    const leftArrows = document.querySelectorAll(".left-arrow");
+    const editGallery = document.getElementById("editGallery");
+
+    modifyMode.addEventListener("click", function () {
+        modal.style.display = "block";
+        overlay.style.display = "block";
+        addPicture.style.display = "none";
+        editGallery.style.display = "block";
+    });
 
 
-  modifyMode.addEventListener("click", function() {
-      modal.style.display = "block";
-      overlay.style.display = "block";
-      addPicture.style.display = "none"; 
-  });
+    closeButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            modal.style.display = "none";
+            overlay.style.display = "none";
+            addPicture.style.display = "none";
+        });
+    });
 
-  
-  closeButtons.forEach(button => {
-      button.addEventListener("click", function() {
-          modal.style.display = "none";
-          overlay.style.display = "none";
-          addPicture.style.display = "none"; 
-      });
-  });
+    leftArrows.forEach(leftArrow => {
+        leftArrow.addEventListener("click", function () {
+            console.log(leftArrow);
+            addPicture.style.display = "none";
+            editGallery.style.display = "block";
+        });
+    });
 
-  overlay.addEventListener("click", function() {
-      modal.style.display = "none";
-      overlay.style.display = "none";
-      addPicture.style.display = "none";
-  });
-
-  document.addEventListener("keydown", function(e) {
-      if (e.key === "Escape") {
-          modal.style.display = "none";
-          overlay.style.display = "none";
-          addPicture.style.display = "none";
-      }
-  });
+    document.addEventListener("keydown", function (e) {
+        if (e.key === "Escape") {
+            modal.style.display = "none";
+            overlay.style.display = "none";
+            addPicture.style.display = "none";
+        }
+    });
 });
